1b5f99c18bb5523c551481eadafb4c4f416489d8
Suzannah Cooper <suzannah.cooper@outlook.com>
2021-07-05 09:14:49 +1000

    Basic outline

--- Classify/__init__.py
+++ Classify/__init__.py
@@ -10,25 +10,28 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
 
     bin_image = req.get_body()
-    #if not bin_image:
-    ##    try:
-     #       req_body = req.get_body()
-     #   except ValueError:
-     #       pass
-     #   else:
-     #       bin_image = req_body.get('image')
+    if not bin_image:
+        try:
+            req_body = req.get_body()
+        except ValueError:
+            pass
+        else:
+            bin_image = req_body.get('image')
 
-    #try:
-    #    image = Image.open(io.BytesIO(bin_image))
-    #    print("returned successfully")
-    #except IOError:
-    #    return func.HttpResponse(
-    #        "Bad input. Unable to cast request body to an image format.",
-    #        status_code=400
-    #    )
+    try:
+        image = Image.open(io.BytesIO(bin_image))
+        print("returned successfully")
+    except IOError:
+        return func.HttpResponse(
+            "Bad input. Unable to cast request body to an image format.",
+            status_code=400
+        )
 
+    # classification
     categories = {"classification": "dog"}
+    # categories = classify_function
 
+    # Convert to json object to send
     try:
         json_object = json.dumps(categories, indent = 4)  
         print("returned successfully")
--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # clothing-classifier-api
+
+# Need to have azure functions installed
+
+# send a binary image as a request
